f555d32a61e47353181395f77ec80de8260b3393
dharvSpring <116044877+dharvSpring@users.noreply.github.com>
2022-11-06 18:04:03 -0600

    implementing foreach, map, filter

---
+++ array methods/myArrayMethods.js
@@ -0,0 +1,48 @@
+
+function myForEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);
+    }
+}
+
+myForEach([1,2,3], function(value, idx, arr) {
+    console.log(`index: ${idx}`);
+    console.log(value ** 2);
+})
+
+
+function myMap(arr, callback) {
+    let retArr = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        retArr[i] = callback(arr[i], i, arr);
+    }
+    return retArr;
+}
+
+const nums = [1, 2, 3, 4, 5];
+console.log(myMap(nums, function(n, i) {
+    return n ** i;
+}))
+
+
+function myFilter(arr, callback) {
+    let filteredArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
+}
+
+console.log(myFilter(nums, function(n, i) {
+    return n < i;
+}))
+
+console.log(myFilter(nums, function(n, i) {
+    return n < 3;
+}))
+
+console.log(myFilter(nums, function(n, i) {
+    return n > 3;
+}))
