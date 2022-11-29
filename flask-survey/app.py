a28377e6ff868c031a63c140ccc50c12b599df62
dharvSpring <david720@gmail.com>
2022-11-28 20:48:36 -0600

    flask survey demo pt 1

---
+++ flask-survey/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "password1"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route("/")
+def show_home():
+    """Display the survey page with the default survey"""
+    return render_template("survey.html", survey=survey)
+
+@app.route("/start", methods=['POST'])
+def start_survey():
+    """Clear response and start survey"""
+    responses = []
+    return redirect("/question/0")
+
+@app.route("/question/<int:qid>")
+def show_question(qid):
+    """Display the requested question for the survery"""
+    if len(responses) != qid:
+        flash("Attempting to access invalid question, redirecting!")
+        return redirect(f"/question/{len(responses)}")
+    if qid >= len(survey.questions):
+        return redirect('/completed')
+
+    cur_q = survey.questions[qid]
+    return render_template("question.html", survey=survey, question=cur_q)
+
+@app.route("/answer", methods=['POST'])
+def answer_question():
+    cur_answer = request.form.get('answer')
+    responses.append(cur_answer)
+
+    if len(responses) >= len(survey.questions):
+        return redirect("/completed")
+
+    return redirect(f"/question/{len(responses)}")
+
+@app.route("/completed")
+def completed_survey():
+    return render_template("completed.html", survey=survey)
