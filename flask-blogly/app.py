fa4cdb3b424692c9f3242f07c5e2d9077b5bbb45
dharvSpring <david720@gmail.com>
2022-12-13 18:59:11 -0600

    functional needs testing

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -9,4 +9,100 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    User.init_data()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def redirect_home():
+    """redirect from / to /users"""
+
+    return redirect("/users")
+
+@app.route("/users/")
+def redirect_users_dir():
+    """redirect from /users/ to /users"""
+
+    return redirect("/users")
+
+@app.route("/users")
+def list_users():
+    """Display a list of all users"""
+
+    # Order by last_name, first_name
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('user_list.html', users=users)
+
+@app.route("/users/new")
+def new_user_form():
+    """Form to create new users"""
+
+    return render_template('new_user.html')
+
+@app.route("/users/new", methods=['POST'])
+def create_new_user():
+    """Create new user"""
+
+    first_name = request.form.get('first-name')
+    last_name = request.form.get('last-name')
+    image_url = request.form.get('image-url')
+    if len(image_url) < 1:
+        image_url = None
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(f"/users/{new_user.id}")
+
+@app.route("/users/<int:user_id>")
+def display_user(user_id):
+    """Display user with user_id"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('user_profile.html', user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def display_user_edit(user_id):
+    """Display edit form for user with user_id"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('edit_user.html', user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=['POST'])
+def edit_user(user_id):
+    """Update user with user_id"""
+
+    first_name = request.form.get('first-name')
+    last_name = request.form.get('last-name')
+    image_url = request.form.get('image-url')
+    if len(image_url) < 1:
+        image_url = User.DEFAULT_PROFILE_IMAGE
+    
+    user = User.query.get_or_404(user_id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(f"/users/{user.id}")
+
+@app.route("/users/<int:user_id>/delete", methods=['POST'])
+def delete_user(user_id):
+    """Delete user with user_id"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    flash(f"Deleted {user.full_name}", "success")
+    
+    return redirect("/users")
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,1 +1,46 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Blogly User"""
+
+    def __repr__(self):
+        """Show info about user."""
+
+        p = self
+        return f"<user {p.id} {p.first_name} {p.last_name} {p.image_url}>"
+
+    def get_full_name(self):
+        """Return the first_name and last_name of the user"""
+
+        return f"{self.first_name} {self.last_name}"
+
+    __tablename__ = "users"
+
+    DEFAULT_PROFILE_IMAGE = "https://www.oseyo.co.uk/wp-content/uploads/2020/05/empty-profile-picture-png-2-2.png"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, default=DEFAULT_PROFILE_IMAGE)
+
+    full_name = property(fget=get_full_name)
+
+    @classmethod
+    def init_data(cls):
+        alan = cls(first_name="Alan", last_name="Alda", image_url="https://s1.stabroeknews.com/images/2019/01/alda.jpg")
+        joel = cls(first_name="Joel", last_name="Burton")
+        jane = cls(first_name="Jane", last_name="Smith")
+        wed = cls(first_name="Wednesday", last_name="Addams", image_url="https://media.boingboing.net/wp-content/uploads/2018/07/Wednesday-Addams.jpg")
+        for user in (alan, joel, jane, wed):
+            db.session.add(user)
+        db.session.commit()
