8ca56b50235951f53c186fcdcbcc02d3f7c1b6d5
dharvSpring <david720@gmail.com>
2022-12-22 17:12:48 -0600

    unittests tag

--- flask-blogly/test_flask.py
+++ flask-blogly/test_flask.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from app import app
-from models import db, User, Post
+from models import db, User, Post, Tag, PostTag
 
 # Use test database and don't clutter tests with SQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -13,14 +13,22 @@ app.config['TESTING'] = True
 # This is a bit of hack, but don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
+def delete_all():
+    """Helper method to clear the tables prior to testing"""
+
+    PostTag.query.delete()
+    Tag.query.delete()
+    Post.query.delete()
+    User.query.delete()
+
 class BloglyUserViewsTestCase(TestCase):
     """Test the user related views"""
     
     def setUp(self):
         """Add a sample user"""
+
         with app.app_context():
-            Post.query.delete()
-            User.query.delete()
+            delete_all()
             self.USER_IMG = "https://64.media.tumblr.com/a175e713588a8cdfc753479a7013c68a/tumblr_oymcoayh5a1sriyo0o1_1280.jpg"
             user = User(first_name="Morticia",
                         last_name="Addams",
@@ -121,10 +129,10 @@ class BloglyPostViewsTestCase(TestCase):
     """Test the post related views"""
     
     def setUp(self):
-        """Add a sample user"""
+        """Add a sample user and post"""
+
         with app.app_context():
-            Post.query.delete()
-            User.query.delete()
+            delete_all()
 
             user = User(first_name="Morticia", last_name="Addams")
             db.session.add(user)
@@ -228,3 +236,107 @@ class BloglyPostViewsTestCase(TestCase):
             
             self.assertIn(f"Deleted post {self.post_title}", html)
             self.assertIn('<ul id="user-list">', html)
+
+class BloglyTagViewsTestCase(TestCase):
+    """Test the tag related views"""
+    
+    def setUp(self):
+        """Add a sample user, post, and tag"""
+
+        with app.app_context():
+            delete_all()
+
+            user = User(first_name="Morticia", last_name="Addams")
+            db.session.add(user)
+            db.session.commit()
+
+            post = Post(title="Sic gorgiamus allos subjectatos nunc", content="Not just pretty words", user_id=user.id)
+            tag = Tag(name="Motto")
+            post.tags.append(tag)
+            db.session.add(post)
+            db.session.commit()
+
+            self.user_id = user.id
+            self.post_id = post.id
+            self.post_title = post.title
+            self.tag_id = tag.id
+            self.tag_name = tag.name
+
+    def tearDown(self):
+        """Clean up any transaction data"""
+
+        with app.app_context():
+            db.session.rollback()
+    
+    def test_tags_list_view(self):
+        """Test tag list"""
+
+        with app.test_client() as client:
+            response = client.get(f"/tags")
+            html = response.get_data(as_text=True)
+
+            self.assertIn('<ul id="tag-list">', html)
+    
+    def test_tags_list_view(self):
+        """Test tag post list"""
+
+        with app.test_client() as client:
+            response = client.get(f"/tags/{self.tag_id}")
+            html = response.get_data(as_text=True)
+
+            self.assertIn('<ul id="post-list">', html)
+            self.assertIn(self.tag_name, html)
+            self.assertIn(self.post_title, html)
+
+    def test_tag_edit_view(self):
+        """Test edit view for tag"""
+
+        with app.test_client() as client:
+            response = client.get(f"/tags/{self.tag_id}/edit")
+            html = response.get_data(as_text=True)
+
+            self.assertIn(self.tag_name, html)
+            self.assertIn(f'<form action="/tags/{self.tag_id}/edit" method="POST">', html)
+    
+    def test_tag_edit_submit(self):
+        """Test edit submit for tag"""
+
+        with app.test_client() as client:
+            data = {"name": "Goals"}
+            response = client.post(f"/tags/{self.tag_id}/edit", data=data, follow_redirects=True)
+            html = response.get_data(as_text=True)
+
+            self.assertNotIn(self.tag_name, html)
+            self.assertNotIn(f'<form action="/tags/{self.tag_id}/edit" method="POST">', html)
+            self.assertIn(f'formaction="/tags/{self.tag_id}/edit"', html)
+            self.assertIn(self.post_title, html)
+    
+    def test_tag_create_view(self):
+        """Test tag create view"""
+        
+        with app.test_client() as client:
+            response = client.get(f"/tags/new")
+            html = response.get_data(as_text=True)
+            self.assertIn(f'<form action="/tags/new" method="POST">', html)
+    
+    def test_tag_create_submit(self):
+        """Test tag create submit"""
+        
+        with app.test_client() as client:
+            data = {"name": "Test Tag"}
+            response = client.post(f"/tags/new", data=data, follow_redirects=True)
+            html = response.get_data(as_text=True)
+            # new data
+            self.assertIn(data['name'], html)
+
+            self.assertNotIn(f'<form action="/tags/new" method="POST">', html)
+
+    def test_tag_delete(self):
+        """Test tag delete"""
+        
+        with app.test_client() as client:
+            response = client.post(f"/tags/{self.tag_id}/delete", follow_redirects=True)
+            html = response.get_data(as_text=True)
+            
+            self.assertIn(f"Deleted tag {self.tag_name}", html)
+            self.assertIn('<ul id="tag-list">', html)
