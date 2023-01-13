e62f273e682ca1f0724e960c684c58d309d2f24a
dharvSpring <david720@gmail.com>
2023-01-12 19:10:07 -0600

    feedback mostly working

--- flask-feedback/app.py
+++ flask-feedback/app.py
@@ -1,8 +1,8 @@
 """Flask app for Feedback"""
 
 from flask import Flask, flash, request, redirect, render_template, session
-from models import db, connect_db, User
-from forms import RegisterUserForm, LoginUserForm
+from models import db, connect_db, User, Feedback
+from forms import RegisterUserForm, LoginUserForm, AddUserFeedbackForm
 # from seed import seed_data
 
 app = Flask(__name__)
@@ -19,11 +19,46 @@ with app.app_context():
 app.config['SECRET_KEY'] = "SECRET!"
 USER_ID_KEY = "username"
 
+def get_authenticated_user():
+    """Return the authenticated user or None"""
+
+    username = session.get(USER_ID_KEY, None)
+    if username == None:
+        return None
+
+    auth_user = User.query.get(username)
+    return auth_user
+
+
+def user_authenticated(username=None):
+    """
+    Helper method to confirm the user specified is the user in the session
+
+    If username is None: return True if any user is in the session
+    """
+
+    if username == None:
+        return session.get(USER_ID_KEY, None) != None
+
+    user = User.query.get_or_404(username)
+    return username == session.get(USER_ID_KEY, None)
+
+def login_error_redirect():
+    """Inform the user they are not authorized for this page and return redirect"""
+    # TODO this could be smarter
+    flash("You must be logged in to view that page!", "danger")
+    return redirect("/login")
+
+
 @app.route("/")
 def get_home():
     """Redirect to /register"""
 
     return redirect("/register")
+
+#
+# Users
+#
 
 @app.route("/register", methods=['GET', 'POST'])
 def handle_register():
@@ -66,27 +101,15 @@ def handle_login():
     
     return render_template("login.html", form=login_form)
 
-@app.route("/secret")
-def get_secret():
-    """The Secret Page"""
+# @app.route("/secret")
+# def get_secret():
+#     """The Secret Page"""
 
-    if session.get(USER_ID_KEY, False):
-        return render_template("secret.html")
-    else:
-        flash("You must be logged in to view the secret page!", "danger")
-        return redirect("/login")
-
-@app.route("/users/<username>")
-def get_user_page(username):
-    """Access the userpage"""
-
-    if session.get(USER_ID_KEY, False):
-        user = User.query.get_or_404(username)
-
-        return render_template("user.html", user=user)
-    else:
-        flash("You must be logged in to view that page!", "danger")
-        return redirect("/login")
+#     if session.get(USER_ID_KEY, False):
+#         return render_template("secret.html")
+#     else:
+#         flash("You must be logged in to view the secret page!", "danger")
+#         return redirect("/login")
 
 @app.route("/logout")
 def logout():
@@ -94,3 +117,101 @@ def logout():
 
     session.pop(USER_ID_KEY)
     return redirect("/")
+
+@app.route("/users/<username>")
+def get_user_page(username):
+    """Access the userpage"""
+    
+    if user_authenticated():
+        user = User.query.get_or_404(username)
+        auth_user=get_authenticated_user()
+
+        return render_template("user.html", user=user, auth_user=auth_user)
+    else:
+        return login_error_redirect()
+
+@app.route("/users/<username>/delete", methods=['POST'])
+def delete_user(username):
+    """Allowed user to delete their account"""
+    # TODO
+    if user_authenticated(username):
+        user = User.query.get_or_404(username)
+        db.session.delete(user)
+        db.session.commit()
+
+        return redirect("/logout")
+    else:
+        return login_error_redirect()
+
+#
+# Feedback
+#
+
+@app.route("/users/<username>/feedback/add", methods=['GET', 'POST'])
+def add_user_feedback(username):
+    """Allow user to add feedback"""
+
+    if user_authenticated(username):
+        feedback_form = AddUserFeedbackForm()
+        if feedback_form.validate_on_submit():
+            new_feedback = Feedback(
+                title=feedback_form.title.data,
+                content=feedback_form.content.data,
+                username=username,
+            )
+            db.session.add(new_feedback)
+            db.session.commit()
+
+            flash("Feedback submitted!", "success")
+            return redirect(f"/users/{username}")
+    
+        return render_template("add_feedback.html", form=feedback_form, username=username)
+    else:
+        return login_error_redirect()
+
+@app.route("/feedback/<int:feedback_id>")
+def show_user_feedback(feedback_id):
+    """Display the user feedback"""
+    
+    feedback = Feedback.query.get_or_404(feedback_id)
+    auth_user = get_authenticated_user()
+
+    return render_template("show_feedback.html", feedback=feedback, auth_user=auth_user)
+
+
+@app.route("/feedback/<int:feedback_id>/update", methods=['GET', 'POST'])
+def update_user_feedback(feedback_id):
+    """User can update their feedback"""
+    
+    feedback = Feedback.query.get_or_404(feedback_id)
+    user = feedback.user
+
+    if user_authenticated(user.username):
+        feedback_form = AddUserFeedbackForm(obj=feedback)
+        if feedback_form.validate_on_submit():
+            feedback.title = feedback_form.title.data
+            feedback.content = feedback_form.content.data
+            db.session.commit()
+
+            flash("Feedback udpated!", "success")
+            return redirect(f"/users/{user.username}")
+    
+        return render_template("update_feedback.html", form=feedback_form, username=user.username)
+    else:
+        return login_error_redirect()
+
+@app.route("/feedback/<int:feedback_id>/delete", methods=['POST'])
+def delete_feedback(feedback_id):
+    """Allowed user to delete their feedback"""
+    
+    feedback = Feedback.query.get_or_404(feedback_id)
+    user = feedback.user
+
+    if user_authenticated(user.username):
+        db.session.delete(feedback)
+        db.session.commit()
+
+        flash("Feedback deleted!", "success")
+        return redirect(f"/users/{user.username}")
+    else:
+        return login_error_redirect()
--- flask-feedback/forms.py
+++ flask-feedback/forms.py
@@ -1,20 +1,26 @@
 """Forms for Feedback"""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, Email, Length
 
 class RegisterUserForm(FlaskForm):
     """Form to register a user"""
     
-    username = StringField("Username", validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired(), Length(max=20)])
     password = PasswordField("Password", validators=[InputRequired()])
-    email = StringField("Email", validators=[InputRequired(), Email()])
-    first_name = StringField("First Name", validators=[InputRequired()])
-    last_name = StringField("Last name", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
+    last_name = StringField("Last name", validators=[InputRequired(), Length(max=30)])
 
 class LoginUserForm(FlaskForm):
     """Form to login a user"""
     
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
+
+class AddUserFeedbackForm(FlaskForm):
+    """Form to add feedback"""
+
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
+    content = TextAreaField("Content", validators=[InputRequired()])
