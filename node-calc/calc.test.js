07432df5a09c1eef70743a06ca18206b3c9eec0f
dharvSpring <david720+springboard@gmail.com>
2023-02-24 04:21:53 -0600

    calcs done, need err handling and tests

---
+++ node-calc/calc.test.js
@@ -0,0 +1,49 @@
+const calc = require('./calc');
+
+describe('Calculation tests', () => {
+
+    test('Count cat in the hat', () => {
+        const mm = new MarkovMachine("the cat in the hat");
+        const mmText = mm.makeText(50);
+        expect(mmText).toEqual(expect.any(String));
+
+        const mmWords = mmText.split(' ');
+        expect(mmWords.length).toEqual(50);
+    });
+
+    test('Verify cat in the hat words', () => {
+        const mm = new MarkovMachine("the cat in the hat");
+        const mmText = mm.makeText(50);
+        const mmWords = mmText.split(' ');
+
+        const expectedWords = "the cat in the hat".split(' ');
+        for (w of mmWords) {
+            expect(expectedWords).toContainEqual(w);
+        }
+
+    }); 
+});
+
+describe('Validation tests', () => {
+
+    test('Count cat in the hat', () => {
+        const mm = new MarkovMachine("the cat in the hat");
+        const mmText = mm.makeText(50);
+        expect(mmText).toEqual(expect.any(String));
+
+        const mmWords = mmText.split(' ');
+        expect(mmWords.length).toEqual(50);
+    });
+
+    test('Verify cat in the hat words', () => {
+        const mm = new MarkovMachine("the cat in the hat");
+        const mmText = mm.makeText(50);
+        const mmWords = mmText.split(' ');
+
+        const expectedWords = "the cat in the hat".split(' ');
+        for (w of mmWords) {
+            expect(expectedWords).toContainEqual(w);
+        }
+
+    }); 
+});
